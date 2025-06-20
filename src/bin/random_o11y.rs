2b9c630b4af8927c312092494008bda4bb7d7e0f
Krishan Mistry <krishan@krishanmistry.com>
2025-06-20 10:43:20 +0100

    WIP: doesn't work yet
    Finding it difficult to get the tracing library spans to just work in Rust due to changes in API

--- src/bin/random_o11y.rs
+++ src/bin/random_o11y.rs
@@ -3,12 +3,13 @@ use std::net::SocketAddr;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper_util::rt::TokioIo;
-use o11y::{handle, init_tracer_provider};
+use o11y::{handle, init_logger_provider, init_tracer_provider};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     init_tracer_provider();
+    init_logger_provider();
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,21 @@ use http_body_util::Full;
 use hyper::Method;
 use hyper::body::Bytes;
 use hyper::{Request, Response};
-use opentelemetry::global::{self, BoxedTracer};
-use opentelemetry::trace::{Span, SpanKind, Status, Tracer};
-use opentelemetry_otlp::SpanExporter;
-use opentelemetry_sdk::propagation::TraceContextPropagator;
-use opentelemetry_sdk::trace::SdkTracerProvider;
+use opentelemetry::KeyValue;
+use opentelemetry::global::{self, BoxedTracer, ObjectSafeTracerProvider};
+use opentelemetry::trace::{Span, SpanKind, Status };
+use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
+use opentelemetry_otlp::{SpanExporter, WithExportConfig};
 use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::logs::SdkLoggerProvider;
+use opentelemetry_sdk::propagation::TraceContextPropagator;
+use opentelemetry_sdk::trace::{SdkTracerProvider};
 use rand::Rng;
-use tracing::info;
+use tracing::{error, info};
+// use tracing_subscriber::fmt::layer;
+use tracing_subscriber::layer::{self, SubscriberExt};
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, Layer};
 
 fn expensive_operation() {
     info!("Starting expensive operation");
@@ -26,16 +33,70 @@ fn get_tracer() -> &'static BoxedTracer {
 }
 
 pub fn init_tracer_provider() {
-    let exporter = SpanExporter::builder()
+    let exporter = opentelemetry_otlp::SpanExporter::builder()
         .with_tonic()
         .build()
         .expect("Failed to create span exporter");
+
     let provider = SdkTracerProvider::builder()
-        .with_resource(Resource::builder().with_service_name("dice_server").build())
+        .with_resource(
+            Resource::builder()
+                .with_service_name(env!("CARGO_PKG_NAME"))
+                .with_attributes([KeyValue::new("service.version", env!("CARGO_PKG_VERSION"))])
+                .build(),
+        )
         .with_batch_exporter(exporter)
         .build();
+
+    
+    let otel_layer = tracing_opentelemetry::layer().;
+    let filter_fmt = EnvFilter::new("info").add_directive("opentelemetry=debug".parse().unwrap());
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_thread_names(true)
+        .with_filter(filter_fmt);
+    tracing_subscriber::registry()
+        .with(otel_layer)
+        .with(fmt_layer)
+        .init();
+    
     global::set_text_map_propagator(TraceContextPropagator::new());
     global::set_tracer_provider(provider);
+}
+
+pub fn init_logger_provider() {
+    //
+
+    let exporter = opentelemetry_stdout::LogExporter::default();
+    let provider = SdkLoggerProvider::builder()
+        .with_resource(
+            Resource::builder()
+                .with_service_name(env!("CARGO_PKG_NAME"))
+                .with_attributes([KeyValue::new("service.version", env!("CARGO_PKG_VERSION"))])
+                .build(),
+        )
+        .with_batch_exporter(exporter)
+        .build();
+
+    let filter_otel = EnvFilter::new("info")
+        .add_directive("hyper=off".parse().unwrap())
+        .add_directive("tonic=off".parse().unwrap())
+        .add_directive("h2=off".parse().unwrap())
+        .add_directive("reqwest=off".parse().unwrap());
+
+    // let otel_layer = OpenTelemetryTracingBridge::new(&provider).with_filter(filter_otel);
+    let otel_layer = tracing_opentelemetry::layer();
+
+    let filter_fmt = EnvFilter::new("info").add_directive("opentelemetry=debug".parse().unwrap());
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_thread_names(true)
+        .with_filter(filter_fmt);
+
+    tracing_subscriber::registry()
+        .with(otel_layer)
+        .with(fmt_layer)
+        .init();
+
+    info!("Logger initialized");
 }
 
 async fn roll_dice(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
@@ -57,6 +118,8 @@ pub async fn handle(
         .span_builder(format!("{} {}", req.method(), req.uri().path()))
         .with_kind(SpanKind::Server)
         .start(tracer);
+
+    error!(name: "my-event-name", target: "my-system", event_id = 20, user_name = "otel", user_email = "otel@opentelemetry.io", message = "This is an example message");
 
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/rolldice") => roll_dice(req).await,
