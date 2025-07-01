0e67f64f178738d0d4c6b2d552f38e8c91a99b18
Krishan Mistry <krishan@krishanmistry.com>
2025-07-01 15:22:26 +0100

    Add manually instrumented logging

--- python-o11y/main.py
+++ python-o11y/main.py
@@ -4,26 +4,36 @@ import time
 
 from fastapi import FastAPI
 from opentelemetry import trace
+from opentelemetry._logs import set_logger_provider
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.semconv.attributes.service_attributes import SERVICE_NAME
 
-LoggingInstrumentor().instrument(set_logging_format=True)
-
 resource = Resource.create({SERVICE_NAME: "python-o11y-service"})
 
-provider = TracerProvider(resource=resource)
-
-otlp_exporter = OTLPSpanExporter(insecure=True)
-span_processor = BatchSpanProcessor(otlp_exporter)
-provider.add_span_processor(span_processor)
-
-trace.set_tracer_provider(provider)
+tracer_provider = TracerProvider(resource=resource)
+otlp_span_exporter = OTLPSpanExporter(insecure=True)
+span_processor = BatchSpanProcessor(otlp_span_exporter)
+tracer_provider.add_span_processor(span_processor)
+trace.set_tracer_provider(tracer_provider)
 tracer = trace.get_tracer(__name__)
+
+logger_provider = LoggerProvider(resource=resource)
+otlp_log_exporter = OTLPLogExporter(insecure=True)
+log_processor = BatchLogRecordProcessor(otlp_log_exporter)
+logger_provider.add_log_record_processor(log_processor)
+handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
+set_logger_provider(logger_provider)
+
+logging.getLogger().setLevel(logging.NOTSET)
+logging.getLogger().addHandler(handler)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -31,6 +41,7 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 logger.error("Lol")
 
+LoggingInstrumentor().instrument(set_logging_format=True)
 FastAPIInstrumentor.instrument_app(app)
 
 
